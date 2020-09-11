024d766222a84c0b73d6398fa538708f047c1f40
prasaneetsomani-22 <64608086+prasaneetsomani-22@users.noreply.github.com>
2020-09-11 19:30:38 +0530

    Update admin.py

--- project_A2/SampleApp/admin.py
+++ project_A2/SampleApp/admin.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import Group
 from .models import Snippet
 from django.urls import path
 from django.http import HttpResponseRedirect
+from django.utils.html import format_html
 
 admin.site.site_header = 'Admin Tutorial Dashboard'
 
 class SnippetAdmin(admin.ModelAdmin):
 	#exclude = ('title',)
 	#fields = ('title',)
-	list_display = ('title','body','created','fontsize')
+	list_display = ('title','body','created','font_size_html_display')
 	list_filter = ('created',)
 	change_list_template = 'admin/snippets/snippets_change_list.html'
 
@@ -25,6 +26,18 @@ class SnippetAdmin(admin.ModelAdmin):
 		self.message_user(request, 'font size set successfully!')
 		return HttpResponseRedirect("../../")
 
+	def font_size_html_display(self, obj):
+		display_size = obj.fontsize if obj.fontsize <= 50 else 50
+		return format_html(
+			f'<span style="font-size:{display_size}px">{obj.fontsize}</span>'
+
+		)
+
+	# def font_size_html_display(self, obj):
+ #        display_size = obj.fontsize if obj.fontsize <=50 else 50
+ #        return format_html(
+ #            f'<span style="font-size:{display_size}px">{obj.fontsize}</span>'
+ #        )
 
 admin.site.register(Snippet,SnippetAdmin)
 admin.site.unregister(Group)
